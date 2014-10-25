0c48d3f2010321438b3a3da1be04d67f95d884a7
ben7th <ben7th@sina.com>
2014-10-26 00:03:49 +0800

    增加基本样式表

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 change-weibo-ui
 ===============
+一个 chrome 浏览器插件，用来调整 weibo 2014 新版的样式
+
+
+#### run
+`ruby -run -e httpd . -p 4000`
---
+++ load_css.js
@@ -0,0 +1,8 @@
+(function(d){
+  var e = d.createElement('link');
+  e.setAttribute('type', 'text/css');
+  e.setAttribute('rel', 'stylesheet');
+  e.setAttribute('charset', 'UTF-8');
+  e.setAttribute('href', 'http://192.168.1.26:4000/weibo.css');
+  document.getElementsByTagName('head')[0].appendChild(e);
+})(document);
